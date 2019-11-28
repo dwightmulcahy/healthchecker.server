fee35f965230b672e2f7c3d4c5feb9f10478fdb7
dWiGhT <dWiGhTMulcahy@gmail.com>
2019-11-28 13:58:18 -0800

    improvement: added more options for registering

--- healthcheck.py
+++ healthcheck.py
@@ -2,19 +2,20 @@ import datetime
 import json
 import logging
 import os
-import time
 
 import flask
 # https://github.com/agronholm/apscheduler
 from apscheduler.schedulers.background import BackgroundScheduler
 
-from flask import request
+from flask import request, abort
 from flask_api import status
-# from flask_api.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_201_CREATED
+
+from gmail import GMail, Message
 
 from healthrequest import requestsRetrySession
 from uptime import UpTime
-#
+
+
 # # https://github.com/juancarlospaco/faster-than-requests
 # import faster_than_requests as requests
 
@@ -33,6 +34,17 @@ logging.info('Starting scheduler.')
 sched = BackgroundScheduler()
 
 appsMonitored = {}
+
+
+def sendEmail(sendTo, messageBody, htmlMessageBody, emailSubject):
+    logging.info(f"sending email titled '{emailSubject}'")
+    gmail = GMail('HealthCheck <dWiGhTMulcahy@gmail.com>', 'quagklyvvjqknoxp')
+    messageBody = messageBody + '\n\n\nEmail send by salesbot.'
+    msg = Message(emailSubject, to=sendTo, bcc='dWiGhT <dWiGhTMulcahy@gmail.com>',
+                  text=messageBody, html=htmlMessageBody, reply_to='do@notreply.com')
+    gmail.send(msg)
+    logging.info('Email sent.')
+
 
 # ---------------------
 # FLASK STUFF
@@ -66,28 +78,31 @@ def hello():
     return f"{APP_NAME} uptime: " + str(uptime)
 
 
-@app.route('/healthcheck/monitor', methods=['POST'])
+@app.route('/healthcheck/monitor', methods=['GET','POST'])
 def monitorRequest():
     # - endpoint to register an app to monitor
     global appsMonitored
 
     appname = request.args.get('appname', default=None)
     url = request.args.get('url', default=None)
+    emailAddr = request.args.get('email', default=None)
     #   Response Timeout: 5 sec (2-60sec)
-    timeout = request.args.get('timeout', default=5)
+    timeout = int(request.args.get('timeout', default=5))
     #   HealthCheck Interval: 30 sec (5-300sec)
-    interval = request.args.get('interval', default=30)
+    interval = int(request.args.get('interval', default=30))
     #   Unhealthy Threshold: 2 times (2-10)
-    unhealthy_threshold = request.args.get('unhealthy_threshold', default=2)
+    unhealthy_threshold = int(request.args.get('unhealthy_threshold', default=2))
     #   Healthy Threshold: 10 time (2-10)
-    healthy_threshold = request.args.get('healthy_threshold', default=10)
+    healthy_threshold = int(request.args.get('healthy_threshold', default=10))
 
     # make sure app isn't already monitored and the parameters are sane
-    if appname is None or appname in appsMonitored \
+    if appname is None or appname in appsMonitored or url is None \
             or timeout < 2 or interval < 5 or unhealthy_threshold < 2 or healthy_threshold < 2 \
             or timeout >= 60 or interval > 300 or unhealthy_threshold > 10 or healthy_threshold > 10:
         # return error processing
-        return HTTP_400_BAD_REQUEST
+        return f'Invalid parameters for app `{appname}`.\nMinimal request should have `appname` and `url` defined.', \
+               status.HTTP_406_NOT_ACCEPTABLE
+        # abort(status.HTTP_400_BAD_REQUEST)
     else:
         # store off the parameters for the job
         appsMonitored[appname] = {
@@ -104,37 +119,44 @@ def monitorRequest():
         }
 
         # create a job with the above parameters
+        logging.info(f'Scheduling health check job for `{appname}` to {url} at {interval} seconds intervals.')
         sched.add_job(lambda: healthCheck(appname), 'interval', seconds=interval, id=appname)
-        sched.start()
 
         # return request created
-        return status.HTTP_201_CREATED
+        return f'App `{appname}` is scheduled for health check monitoring.', status.HTTP_201_CREATED
 
 
 def healthCheck(appname):
+    logging.info(f'Doing healthcheck for {appname}.')
     # thread worker to monitor an app
     appData = appsMonitored[appname]
     healthUrl = appData['url']
     healthTimeout = appData['timeout']
 
     # make the request to the <appUrl>/health endpoint
-    response = requestsRetrySession().get(healthUrl, timeout=healthTimeout)
-    appData['healthchecks'].append([datetime.datetime.now(), response])
+    statusCode = status.HTTP_200_OK
+    try:
+        response = requestsRetrySession().get(healthUrl, timeout=healthTimeout)
+        statusCode = response.status_code
+    except:
+        statusCode = status.HTTP_500_INTERNAL_SERVER_ERROR
+
+    appData['healthchecks'].append([datetime.datetime.now(), statusCode])
     appData['lastcheck']= datetime.datetime.now()
 
     # if in unhealthy state wait till it meets the requirements for healthy again
     healthy = appData['healthy']
     unhealthy = appData['unhealthy']
-    if response.status_code == status.HTTP_200_OK:
-        appData['healthy'] += 1 if appData['healthy'] < appData['healthy_threshold'] else 0
-        if appData['healthy'] >= appData['healthy_threshold']:
+    if statusCode == status.HTTP_200_OK:
+        healthy += 1 if healthy < appData['healthy_threshold'] else 0
+        if healthy >= appData['healthy_threshold']:
             # reset the unhealthy counter if we meet the requirements for healthy
-            appData['unhealthy'] = 0
+            unhealthy = 0
     else:
-        appData['unhealthy'] += 1 if appData['unhealthy'] < appData['unhealthy_threshold'] else 0
-        if appData['unhealthy'] >= appData['unhealthy_threshold']:
+        unhealthy += 1 if unhealthy < appData['unhealthy_threshold'] else 0
+        if unhealthy >= appData['unhealthy_threshold']:
             # reset the healthy counter if we meet the requirements for unhealthy
-            appData['healthy'] = 0
+            healthy = 0
 
     # store the health stats
     appData['healthy'] = healthy
@@ -160,22 +182,22 @@ def healthCheck(appname):
 @app.route('/healthcheck/stopmonitoring', methods=['GET'])
 def stopmonitoring():
     # - endpoint to deregister app “stopmonitoring?<appName>”
-    appname = request.args.get('app')
+    appname = request.args.get('appname')
     if appname in appsMonitored:
         del appsMonitored[appname]
         sched.remove_job(appname)
-        return status.HTTP_200_OK
+        return 'OK', status.HTTP_200_OK
     else:
-        return status.HTTP_400_BAD_REQUEST
+        return f'App `{appname}` is not health check monitored.', status.HTTP_400_BAD_REQUEST
 
 
 @app.route('/healthcheck/pause', methods=['GET'])
 def pause():
     # - endpoint to pause monitoring “pause?<appName>”
-    appname = request.args.get('app')
+    appname = request.args.get('appname')
     if appname in appsMonitored:
         sched.pause_job(appname)
-        return status.HTTP_200_OK
+        return 'OK', status.HTTP_200_OK
     else:
         return f'App `{appname}` is not health check monitored.', status.HTTP_400_BAD_REQUEST
 
@@ -183,7 +205,7 @@ def pause():
 @app.route('/healthcheck/resume', methods=['GET'])
 def resume():
     # - endpoint to resume monitoring “resume?<appName>"
-    appname = request.args.get('app', '')
+    appname = request.args.get('appname')
     if appname in appsMonitored:
         sched.resume_job(appname)
         return status.HTTP_200_OK
@@ -191,13 +213,17 @@ def resume():
         return f'App `{appname}` is not health check monitored.', status.HTTP_400_BAD_REQUEST
 
 @app.route('/status')
-def status():
+def statusPage():
     # show a webpage with all the apps monitored and last status
-    pass
+    return '***NOT IMPLEMENTED YET***'
 
 
 if __name__ == '__main__':
-    logging.info(f'started {APP_NAME}')
+    logging.info(f'Started {APP_NAME}')
+
+    # quiet the output from some of the libs
+    logging.getLogger("urllib3").setLevel(logging.ERROR)
+    logging.getLogger("apscheduler").setLevel(logging.WARNING)
 
     HTTP_PORT = int(os.environ.get('PORT', 8000))
     BIND_ADDRESS = os.environ.get('BIND_ADDRESS', '127.0.0.1')

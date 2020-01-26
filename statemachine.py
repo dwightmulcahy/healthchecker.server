ca467b5395af4f32d239d925de0cd82d2a3f43ca
dWiGhT <dWiGhTMulcahy@gmail.com>
2020-01-26 13:47:30 -0800

    improvement: added more statemachine code, cleanup

--- statemachine.py
+++ statemachine.py
@@ -1,4 +1,6 @@
 import enum
+import logging
+
 from transitions import Machine
 # import graphviz
 # from transitions.extensions import GraphMachine
@@ -14,8 +16,8 @@ class Health(object):
         def __str__(self):
             return str(self.value)
 
-    def __init__(self, appName, unhealthyThreshold = 2, healthyThreshold = 10, debug=False):
-        self.appName = appName
+    def __init__(self, unhealthyThreshold = 2, healthyThreshold = 10, debug = False):
+        self.emailAddr = None
         self.unhealthyChecks = self.healthyChecks = 0
         self.unhealthyThreshold = unhealthyThreshold
         self.healthyThreshold = healthyThreshold
@@ -27,7 +29,7 @@ class Health(object):
             source=[Health.States.UNKNOWN, Health.States.DEGRADING, Health.States.UNHEALTHY],
             dest=Health.States.HEALTHY,
             prepare='incrementHealthy',
-            conditions='isHealthThreshold'
+            conditions='isHealthy'
         )
         self.machine.add_transition(trigger='healthyCheck', source=[Health.States.HEALTHY], dest=None)
 
@@ -36,7 +38,7 @@ class Health(object):
             source=[Health.States.DEGRADING],
             dest=Health.States.UNHEALTHY,
             prepare='incrementUnhealthy',
-            conditions='isUnhealthyThreshold'
+            conditions='isUnhealthy'
         )
         self.machine.add_transition(trigger='unhealthyCheck', source=[Health.States.UNHEALTHY], dest=None)
 
@@ -53,79 +55,121 @@ class Health(object):
         # self.machine.get_graph().draw('my_state_diagram.png', prog='dot')
 
         if debug:
-            self.machine.on_enter_UNKNOWN(lambda: print('entering UNKNOWN'))
-            self.machine.on_enter_DEGRADING(lambda: print('entering DEGRADING'))
-            self.machine.on_enter_UNHEALTHY(lambda: print('entering UNHEALTHY'))
-            self.machine.on_enter_HEALTHY(lambda: print('entering HEALTHY'))
+            # Set transitions' log level to DEBUG, more messages
+            logging.getLogger('transitions').setLevel(logging.DEBUG)
+        else:
+            # Set transitions' log level to ERROR so only important msgs appear
+            logging.getLogger('transitions').setLevel(logging.ERROR)
+
+    def registerEmail(self, appname, emailAddr, emailCallback):
+        # set up state on_enters to email callback
+        self.machine.on_enter_DEGRADING(
+            lambda: emailCallback(
+                sendTo=emailAddr,
+                messageBody=f"`{appname}` has not responded to the last two health checks.",
+                emailSubject=f"`{appname}` health is degraded"
+            )
+        )
+        self.machine.on_enter_HEALTHY(
+            lambda: emailCallback(
+                sendTo=emailAddr,
+                messageBody=f"`{appname}` responded HEALTHY to {self.healthyChecks} health checks.",
+                emailSubject=f"`{appname}` is back to healthy"
+            )
+        )
+        self.machine.on_enter_UNHEALTHY(
+            lambda: emailCallback(
+                sendTo=emailAddr,
+                messageBody=f"`{appname}` is UNHEALTHY for last {self.unhealthyChecks} health checks.",
+                emailSubject=f"`{appname}` is unhealthy"
+            )
+        )
 
     def incrementUnhealthy(self):
-        self.healthyChecks = 0
-        self.unhealthyChecks += 1 if self.unhealthyChecks < self.unhealthyThreshold else 0
+        self.unhealthyChecks += 1 if not self.isUnhealthy() else 0
+        if self.isUnhealthy():
+            self.healthyChecks = 0
 
     def incrementHealthy(self):
-        self.unhealthyChecks = 0
-        self.healthyChecks += 1 if self.healthyChecks < self.healthyThreshold else 0
+        self.healthyChecks += 1 if not self.isHealthy() else 0
+        if self.isHealthy():
+            self.unhealthyChecks = 0
 
-    def isUnhealthyThreshold(self):
+    def isUnhealthy(self):
         return self.unhealthyChecks >= self.unhealthyThreshold
 
     def isDegrading(self):
         return self.unhealthyChecks >= 2
 
-    def isHealthThreshold(self):
+    def isHealthy(self):
         return self.healthyChecks >= self.healthyThreshold
 
 
 if __name__ == '__main__':
-    healthState = Health('test', unhealthyThreshold=4, healthyThreshold=4, debug=True)
+    healthState = Health(unhealthyThreshold=4, healthyThreshold=4, debug=True)
 
+    # initial state machine state
     assert healthState.state == Health.States.UNKNOWN
 
+    # test transition UNKNOWN -> HEALTHY state
     healthState.healthyCheck()
     healthState.healthyCheck()
     healthState.healthyCheck()
     healthState.healthyCheck()
     assert healthState.state == Health.States.HEALTHY
 
+    # make sure the counters are accurate
     assert healthState.healthyChecks == healthState.healthyThreshold
     assert healthState.unhealthyChecks == 0
+
+    # counters shouldn't change after getting to threshold
     healthState.healthyCheck()
     assert healthState.healthyChecks == healthState.healthyThreshold
     assert healthState.unhealthyChecks == 0
 
+    # test transition HEALTHY -> DEGRADING state
     healthState.unhealthyCheck()
     healthState.unhealthyCheck()
     assert healthState.state == Health.States.DEGRADING
 
+    # test transition DEGRADING -> UNHEALTHY state
     healthState.unhealthyCheck()
     healthState.unhealthyCheck()
     assert healthState.state == Health.States.UNHEALTHY
 
-    assert healthState.unhealthyChecks == healthState.unhealthyThreshold
-    assert healthState.healthyChecks == 0
-    healthState.unhealthyCheck()
+    # counters shouldn't change after getting to threshold
     assert healthState.unhealthyChecks == healthState.unhealthyThreshold
     assert healthState.healthyChecks == 0
 
+    # a single healthy check does not mean a state transition yet
+    healthState.healthyCheck()
+    assert healthState.unhealthyChecks == healthState.unhealthyThreshold
+    assert healthState.healthyChecks == 1
+
+    # test transition UNHEALTHY -> HEALTHY state
     healthState.healthyCheck()
     healthState.healthyCheck()
     healthState.healthyCheck()
     healthState.healthyCheck()
     assert healthState.state == Health.States.HEALTHY
 
+    # test transition HEALTHY -> DEGRADING state
     healthState.unhealthyCheck()
     healthState.unhealthyCheck()
     assert healthState.state == Health.States.DEGRADING
 
+    # test transition DEGRADING -> HEALTHY state
     healthState.healthyCheck()
     healthState.healthyCheck()
     healthState.healthyCheck()
     healthState.healthyCheck()
     assert healthState.state == Health.States.HEALTHY
 
+    # test resetting the state to UNKNOWN
     healthState.unknown()
     assert healthState.state == Health.States.UNKNOWN
 
+    # test transition UNKNOWN -> UNHEALTHY state
     healthState.unhealthyCheck()
     healthState.unhealthyCheck()
     assert healthState.state == Health.States.DEGRADING
